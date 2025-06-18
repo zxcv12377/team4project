1a2bab6fa51d795d153bfe8bb8b80dbfcaf221f9
subeen777 <ysb4270@gmail.com>
2025-06-18 16:58:28 +0900

    front

--- frontend/src/extensions/CustomPlaceholder.jsx
+++ frontend/src/extensions/CustomPlaceholder.jsx
@@ -1,5 +1,6 @@
 // src/extensions/CustomPlaceholder.js
 // 에디터가 실제 내용 없이 비어 있을 때만 placeholder 문구를 보여주도록 하는 확장 기능
+// 빈 상태가 플레이스 홀더 문구 가 나타나면 , 텍스트를 입력하면 기능을 제공
 import Placeholder from '@tiptap/extension-placeholder'
 
 const CustomPlaceholder = Placeholder.extend({
