f5f6ddd3666e5f31063b4ef344aa5f17ca9452ed
nahyunwoong <nhw2130050@gmail.com>
2025-06-19 18:24:12 +0900

    asdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,8 @@ import LoginForm from "./components/loginForm";
 
 import RegisterForm from "./components/registerForm";
 import ProtectedRoute from "./components/protectedRoute";
-<<<<<<< HEAD
 import Navbar from "./components/navbar";
 import UpdateMyProfile from "./components/UpdateMyProfile";
-=======
-import { BoardList } from "./components/Board";
->>>>>>> b4e4d5ea99d8d75b248601aa82387517712344a4
 
 function App() {
   return (
@@ -22,16 +18,9 @@ function App() {
 
         <Route element={<Navbar />}>
           <Route path="/" element={<Navigate to="/board" />} />
-<<<<<<< HEAD
           <Route path="/board" element={<BoardPage />} />
           <Route path="/reply" element={<ReplyList />} />
           <Route path="UpdateProfile" element={<UpdateMyProfile />} />
-=======
-          <Route path="/board" element={<BoardList />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-          {/* 보호된 라우트(로그인 인증 후 접근 가능한 경로 지정) */}
->>>>>>> b4e4d5ea99d8d75b248601aa82387517712344a4
           <Route
             path="/profile"
             element={
