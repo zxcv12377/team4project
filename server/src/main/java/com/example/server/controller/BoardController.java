681c51aa9366a69b4a8c38635340c9edd0f81588
subeen777 <ysb4270@gmail.com>
2025-06-19 15:11:49 +0900

    garbage

--- server/src/main/java/com/example/server/controller/BoardController.java
+++ server/src/main/java/com/example/server/controller/BoardController.java
@@ -1,25 +1,14 @@
 package com.example.server.controller;
 
-import java.lang.ProcessBuilder.Redirect;
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.server.dto.BoardDTO;
 import com.example.server.dto.PageRequestDTO;
@@ -31,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RequestMapping("/api/board")
-@Controller
+@RestController
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
