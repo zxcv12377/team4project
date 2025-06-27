3bcd301565d5a80b636afe54459acab0f26b5c96
ShimHyunJin <zxcv12377@naver.com>
2025-06-27 14:47:02 +0900

    음성채널 나가는 로직 구현

---
+++ frontend/src/components/voice/VoiceChannelOuter.jsx
@@ -0,0 +1,26 @@
+import { socket } from "../../lib/socket";
+
+const LeaveVoiceButton = ({ roomId, member, joined, onLeave }) => {
+  const handleLeave = () => {
+    socket.emit("leaveRoom", roomId); // 서버에 퇴장 알림
+    console.log(`${member?.name} 님이 퇴장했습니다.`);
+
+    socket.removeAllListeners("newProducer");
+    socket.removeAllListeners("speaking-users");
+    onLeave();
+  };
+
+  return (
+    <div>
+      {joined && (
+        <div className="flex justify-center gap-2 px-2 py-2 rounded hover:bg-red-600 group cursor-pointer bg-red-500 mb-2">
+          <button onClick={handleLeave} className="text-center">
+            나가기
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LeaveVoiceButton;
